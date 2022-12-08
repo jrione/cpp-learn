6ce1393af1699a82ad2c3b6ea6e1c64bc4f36758
Pajri Zahrawaani Ahmad <pajriblueholigand@gmail.com>
2022-12-08 12:33:30 +0700

    edit progress

--- cpp-learn/konversi-suhu.cpp
+++ cpp-learn/konversi-suhu.cpp
@@ -12,31 +12,55 @@ using namespace std;
 bool setFail;
 string unit[] = { "C","R","F","K" };
 
-string implode(vector<string> strings, string delim)
-{
+string implode(vector<string> strings, string delim) {
     stringstream ss;
     copy(strings.begin(), strings.end(),
         ostream_iterator<string>(ss, delim.c_str()));
     return ss.str();
 }
 
-float convert(int nominal,string fromScale, string toScale) {
-    vector<string> FromTo = { fromScale, toScale};
-    map<string,float> rumus {
-        {"C_R_", nominal*4/5},
-        {"R_R_", nominal},
-        {"F_R_", (nominal-32)*4/9},
-        {"K_R_", (nominal-273)*(4/5)}
-    };
 
-    string sumScale =  implode(FromTo, "_");
-    return rumus[sumScale];
+void convert(int nominal,string fromScale, string toScale) {
+    string FromTo = implode({ fromScale, toScale}, "_");
+    string ToFrom = implode({ toScale, fromScale }, "_");
+    map<string, map<string, int> > rumus;
 
+
+    for (int j = 0; j < sizeof(unit)/sizeof(unit[0]); j++) { //baris CRFK
+        rumus[unit[j]];
+        for (int k = 0; k < sizeof(unit) / sizeof(unit[0]); k++) { //kolom cc,cr,cf,ck,dsb
+            if (fromScale == toScale) {
+                rumus[fromScale][FromTo] = nominal;
+            }
+            else {
+                rumus[unit[j]][implode({ unit[j], unit[k]}, "_")];
+            }
+        }
+        for (auto a : rumus[unit[j]]) {
+            if (a.first == FromTo || a.first == ToFrom) {
+                cout << a.first <<": "<<a.second<< endl;
+            }
+        }
+    }
+
+    
+
+   /* for (int m = 0; m < sizeof(unit) / sizeof(unit[0]); m++) {
+        for (auto l : rumus[unit[m]]) cout << '{' << l.first << ", " << l.second << "}\n";
+    }
+    */
+    //string sumScale =  implode(FromTo, "_");
+    //return rumus[sumScale];
+
+    /*{"C_R_", nominal * 4 / 5},
+    { "R_R_", nominal },
+    { "F_R_", (nominal - 32) * 4 / 9 },
+    { "K_R_", (nominal - 273) * 4 / 5 }*/
 }
 
 
  void menu() {
-     string allScale[2];
+    string allScale[2];
     int nominalSuhu;
 
     cout << "KONVERSI SUHU" << endl;
@@ -51,14 +75,18 @@ float convert(int nominal,string fromScale, string toScale) {
     else {
         cout << "Pilih skala suhu yang dikonversi: "; cin >> allScale[0];
         cout << "Pilih skala suhu target konversi: "; cin >> allScale[1];
-        
+
         bool exist[2];
-        for (int i = 0; i < sizeof(allScale)/sizeof(allScale[0]); i++) {
-            for (char chara: allScale[i]) allScale[i] = toupper(chara);
+        for (int i = 0; i < sizeof(allScale) / sizeof(allScale[0]); i++) {
+            for (char chara : allScale[i]) allScale[i] = toupper(chara);
             exist[i] = find(begin(unit), end(unit), allScale[i]) != end(unit);
         }
 
-        if (exist[0] == true && exist[1] == true) {
+    }
+    convert(nominalSuhu, allScale[0], allScale[1]);
+
+
+       /* if (exist[0] == true && exist[1] == true) {
             float sum;
             sum = convert(nominalSuhu, allScale[0], allScale[1]);
             cout << "============================================" << endl;
@@ -68,11 +96,7 @@ float convert(int nominal,string fromScale, string toScale) {
         else {
             cout << "Masukkan input sesuai INGFO!";
             setFail = true;
-        }
-
-
-    }
-
+        }*/
 }
 
 int main() {
@@ -90,14 +114,15 @@ int main() {
     isAgain:
         cout << "============================================" << endl;
         cout << "Ulangi? (y/n): "; cin >> again;
-        cout << "============================================" << endl;
         if (again == 'y' || again == 'Y') {
+            system("CLS");
             goto mainMenu;
         }
         else {
             goto exit;
         }
     exit:
+        cout << "============================================" << endl;
         cout << "Terima kasih :)" << endl;
         return 0;
 }
